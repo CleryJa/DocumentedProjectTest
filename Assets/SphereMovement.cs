15b7c820d2d3d78ebe0c259cbdf047cecaebef85
Jacques <cleryja@gmail.com>
2021-04-23 14:37:40 +0200

    Initial commit

---
+++ Assets/SphereMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMovement : MonoBehaviour
+{
+    /// <summary>
+    /// Move the sphere et dit salut
+    /// </summary>
+    public void SphereMove()
+    {
+        Debug.Log("Salut");
+    }
+
+    /// <summary>
+    /// Privé coucou
+    /// </summary>
+    private void PrivateSphereMove()
+    {
+        Debug.Log("Coucou");
+    }
+}
